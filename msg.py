95ae964cac794e58aa3dff2d3edac00f6219aa06
MrUknownm <168353967+MrUknownm@users.noreply.github.com>
2024-05-18 23:05:12 +0300

    Add files via upload

---
+++ msg.py
@@ -0,0 +1,14 @@
+#v.01
+#Author: Sjlala
+
+#Libraries
+import pyautogui as auto
+import time
+
+#Spammer
+time.sleep(4)
+while True:
+    auto.write('DEN PAIRNEI H AEK PROTA8LIME MIRI') #Text it spams
+    auto.press('enter')
+    time.sleep(1)
+
